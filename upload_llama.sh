3bf9b1d42071c1f2fd32a27ac048fcda692fea49
Rob174 <robin.128.456@gmail.com>
2023-10-13 23:27:16 -0400

    shell script to print the commands to upload the files to the server

---
+++ upload_llama.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+server=cedar
+root=/scratch
+user=rmoine
+
+echo ""
+echo "scp -r ./src/llm/llama/*.py rmoine@$server.computecanada.ca:$root/$user/"
+
+# echo ""
+echo "scp ./src/llm/llama/severity_launch rmoine@$server.computecanada.ca:$root/$user/"
+
+echo 'ssh rmoine@$server.computecanada.ca" "cd $root/$user/&&mkdir data&&mkdir data/"'
+
+# echo ""
+echo "scp -r ./data/llm/data_preprocessed_tokens_v2.json rmoine@$server.computecanada.ca:$root/$user/data/"
