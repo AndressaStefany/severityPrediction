d19ad76f3fc96135967bcd8c98526753f890041e
Rob174 <robin.128.456@gmail.com>
2023-10-16 12:13:13 -0400

    update upload script; python script to generate launch file

---
+++ src/llm/llama/generate_severities.py
@@ -0,0 +1,34 @@
+from string import Template
+from pathlib import Path
+
+
+def generate_inference(clear: bool = False):
+    n_chunks = 10
+    id = "_trunc"
+    path_file = Path(__file__)
+    path_template = (
+        path_file.parent.parent.parent.parent
+        / "data"
+        / "templates"
+        / "template_inference.txt"
+    )
+    with open(path_template) as f:
+        template = Template(f.read())
+    if clear:
+        for f in path_file.parent.rglob("inference_*"):
+            f.unlink()
+    for i in range(n_chunks):
+        with open(path_file.parent / f"inference{id}_{i}", "w") as f:
+            f.write(
+                template.substitute(
+                    id=i,
+                    n_chunks=n_chunks,
+                    path_data_json="/project/def-aloise/$USER/data/data_preprocessed_tokens_v3.json",
+                )
+            )
+    with open(path_file.parent / f"launch{id}_all_inference", "w") as f:
+        f.write("\n".join([f"sbatch ./inference{id}_{i}" for i in range(n_chunks)]))
+
+
+if __name__ == "__main__":
+    generate_inference()
--- upload_llama.sh
+++ upload_llama.sh
@@ -2,14 +2,19 @@
 server=cedar
 root=/project/def-aloise
 user=rmoine
+root_project=/mnt/c/Users/robin/Documents/projets/severityPrediction
 
 echo ""
-echo "scp -r ./src/llm/llama/*.py rmoine@$server.computecanada.ca:$root/$user/"
+echo "scp -r $root_project/src/llm/llama/*.py rmoine@$server.computecanada.ca:$root/$user/"
 
 # echo ""
-echo "scp ./src/llm/llama/severity_launch rmoine@$server.computecanada.ca:$root/$user/"
+echo "scp $root_project/src/llm/llama/severity_launch rmoine@$server.computecanada.ca:$root/$user/"
+echo "scp $root_project/src/llm/llama/inference_* rmoine@$server.computecanada.ca:$root/$user/"
+echo "scp $root_project/src/llm/llama/launch_* rmoine@$server.computecanada.ca:$root/$user/"
 
 echo 'ssh rmoine@$server.computecanada.ca" "cd $root/$user/&&mkdir data&&mkdir data/"'
 
 # echo ""
-echo "scp -r ./data/llm/data_preprocessed_tokens_v2.json rmoine@$server.computecanada.ca:$root/$user/data/"
+echo "scp -r $root_project/data/llm/data_preprocessed_tokens_v3.json rmoine@$server.computecanada.ca:$root/$user/data/"
+
+echo "scp -r $root_project/data/llm/data_preprocessed_tokens_v2.json rmoine@$server.computecanada.ca:$root/$user/data/"
