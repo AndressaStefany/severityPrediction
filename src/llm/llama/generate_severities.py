2df99bce04f1d1d58219f05177cc6b3b303cdd0f
Rob174 <robin.128.456@gmail.com>
2023-11-29 11:57:15 -0500

    corrections submission batches and cancel multiple in launch scripts server

--- src/llm/llama/generate_severities.py
+++ src/llm/llama/generate_severities.py
@@ -3,11 +3,9 @@ from pathlib import Path
 import itertools as it
 import random
 
+
 def generate_inference(clear: bool = False):
-    dataset_choices = [
-        "eclipse_72k",
-        "mozilla_200k"
-    ]
+    dataset_choices = ["eclipse_72k", "mozilla_200k"]
     n_chunks = 10
     path_file = Path(__file__)
     path_template = (
@@ -17,17 +15,19 @@ def generate_inference(clear: bool = False):
         / "template_inference.txt"
     )
     folder_out = path_file.parent / "launches" / "inference"
-    folder_out.mkdir(parents=True,exist_ok=True)
+    folder_out.mkdir(parents=True, exist_ok=True)
     with open(path_template) as f:
         template = Template(f.read())
     if clear:
         for f in path_file.parent.rglob("inference_*"):
             f.unlink()
-    root = "/project/def-aloise/rmoine/launches/finetune/"
+    root = "/project/def-aloise/rmoine/launches/inference/"
     Llaunches = ["#!/bin/bash"]
+    i = 0
     for dataset_choice in dataset_choices:
         for interval_idx in range(n_chunks):
-            name = f"inference_{interval_idx}"
+            name = f"inference_{i}"
+            i += 1
             with open(folder_out / name, "w") as f:
                 f.write(
                     template.substitute(
@@ -38,21 +38,20 @@ def generate_inference(clear: bool = False):
                 )
             Llaunches.append(name)
     with open(folder_out / f"launch_inference", "w") as f:
-        f.write("\n".join([f"sb "+root+e for e in Llaunches]))
+        f.write("\n".join([f"sbatch " + root + e+";" if i > 0 else e for i,e in enumerate(Llaunches)]))
     with open(folder_out / f"cancel_inference", "w") as f:
-        f.write("\n".join([f"scancel -n "+root+e for e in Llaunches]))
+        f.write("\n".join([f"scancel -n " + root + e+";" if i > 0 else e for i,e in enumerate(Llaunches)]))
 
 
 def generate_embeddings(clear: bool = False):
     choice = 0
-    dataset_choices = [
-        "eclipse_72k",
-        "mozilla_200k"
-    ]
+    dataset_choices = ["eclipse_72k", "mozilla_200k"]
     n_chunks = 10
     model_name = "meta-llama/Llama-2-13b-chat-hf"
     dataset_choice = dataset_choices[choice]
-    limit_tokens = 1000 # 1104 with hello word limit but seems to be not short enough...
+    limit_tokens = (
+        1000  # 1104 with hello word limit but seems to be not short enough...
+    )
     path_file = Path(__file__)
     path_folder_out = path_file.parent / "launches" / "embeddings"
     path_template = (
@@ -80,12 +79,22 @@ def generate_embeddings(clear: bool = False):
             )
     with open(path_folder_out / f"start_emb_{dataset_choice}", "w") as f:
         f.write(
-            "#/bin/bash\n"+"\n".join([f"sbatch /project/def-aloise/rmoine/launches/embeddings/emb_{i}_{dataset_choice}" for i in range(n_chunks)])
+            "#/bin/bash\n"
+            + "\n".join(
+                [
+                    f"sbatch /project/def-aloise/rmoine/launches/embeddings/emb_{i}_{dataset_choice}"
+                    for i in range(n_chunks)
+                ]
+            )
         )
     with open(path_folder_out / f"stop_emb_{dataset_choice}", "w") as f:
         f.write(
-            "#/bin/bash\n"+"\n".join([f"scancel -n emb_{i}_{dataset_choice}" for i in range(n_chunks)])
+            "#/bin/bash\n"
+            + "\n".join(
+                [f"scancel -n emb_{i}_{dataset_choice}" for i in range(n_chunks)]
+            )
         )
+
 
 def cartesian_product_dict(**kwargs):
     keys = kwargs.keys()
@@ -99,14 +108,15 @@ def cartesian_product_dict(**kwargs):
 
     return result
 
+
 def generate_finetune(clear: bool = False):
     random.seed(0)
-    dataset_choices = ["eclipse_72k","mozilla_200k"]
-    learning_rate = [1e-4,1e-5]
-    lora_r = [10,64,5]
-    weighted = [True,False]
-    lora_alpha = [4,10]
-    lora_dropout = [0.1,0,0.2]
+    dataset_choices = ["eclipse_72k", "mozilla_200k"]
+    learning_rate = [1e-4, 1e-5]
+    lora_r = [10, 64, 5]
+    weighted = [True, False]
+    lora_alpha = [4, 10]
+    lora_dropout = [0.1, 0, 0.2]
     tr_weighted_sampling = [False, True]
     parameters = cartesian_product_dict(
         learning_rate=learning_rate,
@@ -137,23 +147,19 @@ def generate_finetune(clear: bool = False):
     i = 0
     Llaunches = ["#!/bin/bash"]
     root = "/project/def-aloise/rmoine/launches/finetune/"
-    root_local = path_file.parent / "launches"/ "finetune"
+    root_local = path_file.parent / "launches" / "finetune"
     root_local.mkdir(exist_ok=True)
     for dataset_choice in dataset_choices:
         for p in parameters:
             with open(root_local / f"finetune{i}", "w") as f:
-                f.write(
-                    template.substitute(
-                        dataset_choice=dataset_choice,
-                        **p
-                    )
-                )
+                f.write(template.substitute(dataset_choice=dataset_choice, **p))
             Llaunches.append(f" {root}finetune{i}")
             i += 1
     with open(root_local / "launch_all_finetune", "w") as fp:
-        fp.write("\n".join(["sb"+ e for e in Llaunches]))
-    with open(root_local / "launch_all_finetune", "w") as fp:
-        fp.write("\n".join(["scancel -n"+ e for e in Llaunches]))
+        fp.write("\n".join(["sbatch" + e+";" if i > 0 else e for i,e in enumerate(Llaunches)]))
+    with open(root_local / "cancel_all_finetune", "w") as fp:
+        fp.write("\n".join(["scancel -n" + e+";" if i > 0 else e  for i,e in enumerate(Llaunches)]))
+
 
 if __name__ == "__main__":
     generate_inference()
