a54b4d7f243c92136f7c57c488c3024bca5387a0
Rob174 <robin.128.456@gmail.com>
2024-03-30 16:51:03 -0400

    results: fientuning results

--- data/finetuning_v3/pull.sh
+++ data/finetuning_v3/pull.sh
@@ -1,5 +1,6 @@
 #/bin/bash
-server=narval
+server=graham
 root_path=/project/def-aloise/rmoine/data
 addr=rmoine@$server.computecanada.ca:
-rsync -avz --info=progress2 $addr$root_path/qlora_finetune_* .
+# rsync -avz --info=progress2 $addr$root_path/qlora_finetune_* .
+rsync -rltv --info=progress2 $addr$root_path/qlora_finetune_*redo .
